a27ac970759671cc56da3b0cdc4ea256761d9437
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-08 11:23:16 +0000

    Returns 'Fizzbuzz' - passing the test

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,8 @@
 def fizz_buzz(x):
-  if x % 3 == 0:
-    return 'Fizz'
-  if x % 5 == 0:
+  if x % 5 == 0 and x % 3 == 0:
+    return 'Fizzbuzz'
+  elif x % 5 == 0:
     return 'buzz'
+  elif x % 3 == 0:
+    return 'Fizz'
   pass
