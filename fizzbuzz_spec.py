efc478b49f88506ce136ac8e9330459d1c8b4fe9
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-08 11:22:46 +0000

    Test: It fizzbuzzes on 15

--- fizzbuzz_spec.py
+++ fizzbuzz_spec.py
@@ -4,3 +4,5 @@ def test_answer():
     assert fizzbuzz.fizz_buzz(3) == 'Fizz'
 def test_answer():
     assert fizzbuzz.fizz_buzz(5) == 'buzz'
+def test_answer():
+    assert fizzbuzz.fizz_buzz(15) == 'Fizzbuzz'
